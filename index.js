f6c5fd5679961bfbcb5a43110010307a339eb2fc
Martijn Hanrath <martijnjwvhanrath@gmail.com>
2019-03-05 18:06:29 +0100

    add: update package.json, index runnable, sample .env

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 'use strict';
 
-console.info('Aloha, world!');
+const env = require('dotenv').config();
+
+if (env.error) {
+  throw new Error('Invalid environment configuration');
+}
+
+const appConfig = env.parsed;
+
+async function start(config) {
+  const { URL, OUTPUT_DIR } = config;
+  const pinterestRssFeedImageDownloader = require('./index-factory')(URL, OUTPUT_DIR);
+
+  await pinterestRssFeedImageDownloader
+    .then(console.info)
+    .catch(err => {
+      console.error(err);
+    });
+}
+
+start(appConfig);
